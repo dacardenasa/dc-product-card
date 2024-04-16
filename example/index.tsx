9cab1fd04ed24c7bb673a830c344924990886507
Diego Cardenas <diego.cardenasaleg@gmail.com>
2024-04-16 14:24:27 -0500

    upgrade version to 1.0.1-rc and updated module imports in package.json

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ProductCard, ProductImage, ProductButtons, ProductTitle } from '../.';
+import { ProductCard, ProductImage, ProductButtons, ProductTitle } from '../';
 
 const product =  {
   id: "1",
@@ -14,7 +14,7 @@ const App = () => {
       product={product}
       initialValues={{ count: 6, maxCount: 10 }}
     >
-      {({ isMaxCountReached, handleIncreaseBy, reset }) => (
+      {() => (
         <>
           <ProductImage />
           <ProductTitle />
